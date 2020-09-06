6cd2a19cd9a7b7721f1f1d95398718b2bc41b868
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-09-06 23:24:47 +0200

    Tested a resolver for safety reasons.

---
+++ src/project/resolvers.test.ts
@@ -0,0 +1,42 @@
+import { init } from '../init'
+import { insert, toSQLDate } from '../misc/dbUtils'
+import { getFakeUser } from '../test/getFakeUser'
+import { getFakeClient } from '../test/getFakeClient'
+import { getFakeProject } from '../test/getFakeProject'
+import resolvers from './resolvers'
+import { getDatabase } from '../misc/getDatabase'
+import { User } from '../user/User'
+import SQL from 'sql-template-strings'
+
+describe('project resolvers', () => {
+  beforeAll(async () => {
+    await init()
+  })
+
+  describe('User', () => {
+    describe('cashedBalance', () => {
+      it('should work', async () => {
+        const { lastID: userId } = await insert('user', getFakeUser())
+        const { lastID: clientId } = await insert('client', getFakeClient({ user: userId }))
+
+        await insert('project', getFakeProject({
+          client: clientId,
+          cashed_at: toSQLDate(new Date()),
+          cashed_balance: 15
+        }))
+
+        await insert('project', getFakeProject({
+          client: clientId,
+          cashed_at: toSQLDate(new Date()),
+          cashed_balance: 25
+        }))
+
+        const db = await getDatabase()
+        const user = (await db.get<User>(SQL`SELECT * FROM user WHERE id = ${userId}`))!
+        const cashedBalance = await resolvers.User.cashedBalance(user, { user }, {}, null as any)
+
+        expect(cashedBalance).toBe(40)
+      })
+    })
+  })
+})
