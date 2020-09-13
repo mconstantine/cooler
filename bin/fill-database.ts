bfdbcfb6e2645003934c8dd0aabbaa2d21809840
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-09-13 22:15:53 +0200

    Updated fill-database script.

--- bin/fill-database.ts
+++ bin/fill-database.ts
@@ -3,24 +3,32 @@ import { init } from '../src/init'
 import { getDatabase } from '../src/misc/getDatabase'
 import { createUser } from '../src/user/model'
 import { verify } from 'jsonwebtoken'
-import { Token, User } from '../src/user/User'
 import SQL from 'sql-template-strings'
 import { getFakeProject } from '../src/test/getFakeProject'
 import { getFakeClient } from '../src/test/getFakeClient'
 import { getFakeTask } from '../src/test/getFakeTask'
-import { getFakeSession } from '../src/test/getFakeSession'
+import { getFakeSessionFromDatabase } from '../src/test/getFakeSession'
 import { fromSQLDate, toSQLDate, insert } from '../src/misc/dbUtils'
 import { getFakeTax } from '../src/test/getFakeTax'
-
-(async () => {
+import { Token, UserFromDatabase } from '../src/user/interface'
+import { Tax } from '../src/tax/interface'
+import { ProjectFromDatabase } from '../src/project/interface'
+import { TaskFromDatabase } from '../src/task/interface'
+import { toDatabase as taskToDatabase } from '../src/task/model'
+import { SessionFromDatabase } from '../src/session/interface'
+;(async () => {
   const USERS_COUNT = 2
   const CLIENTS_PER_USER = 20
   const PROJECTS_PER_CLIENT = 3
   const TASKS_PER_PROJECT = 10
   const SESSIONS_PER_TASK = 10
 
-  const totalProgress = USERS_COUNT * CLIENTS_PER_USER * PROJECTS_PER_CLIENT *
-    TASKS_PER_PROJECT * SESSIONS_PER_TASK
+  const totalProgress =
+    USERS_COUNT *
+    CLIENTS_PER_USER *
+    PROJECTS_PER_CLIENT *
+    TASKS_PER_PROJECT *
+    SESSIONS_PER_TASK
 
   console.log(`Will create ${totalProgress} entities. This could take a while.`)
   dotenv.config()
@@ -30,34 +38,42 @@ import { getFakeTax } from '../src/test/getFakeTax'
   await init()
   await db.exec(`DELETE FROM user`)
 
-  const { accessToken } = await createUser({
-    name: 'Mauro Constantinescu',
-    email: 'mauro.constantinescu@gmail.com',
-    password: 'password'
-  }, { user: null })
+  const { accessToken } = await createUser(
+    {
+      name: 'Mauro Constantinescu',
+      email: 'mauro.constantinescu@gmail.com',
+      password: 'password'
+    },
+    {}
+  )
 
   const token = verify(accessToken, process.env.SECRET!) as Token
-  const user = await db.get(SQL`SELECT * FROM user WHERE id = ${token.id}`) as User
+  const user = await db.get<UserFromDatabase>(
+    SQL`SELECT * FROM user WHERE id = ${token.id}`
+  )
 
-  await createUser({
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    password: 'password'
-  }, { user })
+  await createUser(
+    {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'password'
+    },
+    { user }
+  )
 
-  const users = await db.all<User[]>('SELECT * FROM user')
+  const users = await db.all<UserFromDatabase[]>('SELECT * FROM user')
 
   for (let u = 0; u < users.length; u++) {
     const user = users[u]
 
     for (let t = 0; t < 3; t++) {
-      await insert('tax', getFakeTax({ user: user.id }))
+      await insert<Partial<Tax>>('tax', getFakeTax(user.id))
     }
 
     const clients: number[] = []
 
     for (let c = 0; c < CLIENTS_PER_USER; c++) {
-      const { lastID } = await insert('client', { ...getFakeClient({ user: user.id }) })
+      const { lastID } = await insert('client', getFakeClient(user.id))
       clients.push(lastID!)
     }
 
@@ -66,7 +82,11 @@ import { getFakeTax } from '../src/test/getFakeTax'
       const projects: number[] = []
 
       for (let p = 0; p < PROJECTS_PER_CLIENT; p++) {
-        const { lastID } = await insert('project', getFakeProject({ client }))
+        const { lastID } = await insert<Partial<ProjectFromDatabase>>(
+          'project',
+          getFakeProject(client)
+        )
+
         projects.push(lastID!)
       }
 
@@ -77,11 +97,15 @@ import { getFakeTax } from '../src/test/getFakeTax'
         const expectedWorkingHours: number[] = []
 
         for (let t = 0; t < TASKS_PER_PROJECT; t++) {
-          const taskData = getFakeTask({ project })
-          const { lastID } = await insert('task', taskData)
+          const task = getFakeTask(project)
 
-          expectedWorkingHours.push(taskData.expectedWorkingHours!)
-          startTime.push(fromSQLDate(taskData.start_time!))
+          const { lastID } = await insert<Partial<TaskFromDatabase>>(
+            'task',
+            taskToDatabase(task)
+          )
+
+          expectedWorkingHours.push(task.expectedWorkingHours!)
+          startTime.push(task.start_time)
           tasks.push(lastID!)
         }
 
@@ -92,38 +116,57 @@ import { getFakeTax } from '../src/test/getFakeTax'
           const sessions: number[] = []
 
           for (let s = 0; s < SESSIONS_PER_TASK; s++) {
-            const session = getFakeSession({
-              task,
-              start_time: toSQLDate(new Date(
-                st.getTime() + 1000 + Math.round(Math.random() * 86400000)
-              ))
+            const session = getFakeSessionFromDatabase(task, {
+              start_time: toSQLDate(
+                new Date(
+                  st.getTime() + 1000 + Math.round(Math.random() * 86400000)
+                )
+              )
             })
-
-            const startTime = fromSQLDate(session.start_time!)
 
             // 20% to 120%
             const workingHours = ewh * 0.2 + Math.random() * ewh * 1
-            session.end_time = toSQLDate(new Date(startTime.getTime() + 3600000 * workingHours))
 
-            const {lastID} = await insert('session', session)
+            session.end_time = toSQLDate(
+              new Date(
+                fromSQLDate(session.start_time).getTime() +
+                  3600000 * workingHours
+              )
+            )
+
+            const { lastID } = await insert<Partial<SessionFromDatabase>>(
+              'session',
+              session
+            )
+
             sessions.push(lastID!)
 
-            const currentProgress = (
-              u * CLIENTS_PER_USER * PROJECTS_PER_CLIENT * TASKS_PER_PROJECT * SESSIONS_PER_TASK +
+            const currentProgress =
+              u *
+                CLIENTS_PER_USER *
+                PROJECTS_PER_CLIENT *
+                TASKS_PER_PROJECT *
+                SESSIONS_PER_TASK +
               c * PROJECTS_PER_CLIENT * TASKS_PER_PROJECT * SESSIONS_PER_TASK +
               p * TASKS_PER_PROJECT * SESSIONS_PER_TASK +
               t * SESSIONS_PER_TASK +
               s
-            )
 
             process.stdout.clearLine(0)
             process.stdout.cursorTo(0)
-            process.stdout.write(`Working...${Math.round(currentProgress / totalProgress * 100)}%`)
+            process.stdout.write(
+              `Working...${Math.round(
+                (currentProgress / totalProgress) * 100
+              )}%`
+            )
           }
         }
       }
     }
   }
 })()
-.then(() => process.exit())
-.catch(e => { console.log(e); process.exit() })
+  .then(() => process.exit())
+  .catch(e => {
+    console.log(e)
+    process.exit()
+  })
