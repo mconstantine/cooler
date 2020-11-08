d8e361afbf96b7f733c933aa84606d23aab03aa6
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-11-08 23:36:48 +0100

    Created pipeLog util.

--- src/test/util.ts
+++ src/test/util.ts
@@ -4,3 +4,8 @@ import { coolerError } from '../misc/Types'
 export function testError(): ApolloError {
   return coolerError('COOLER_500', 'Test failed')
 }
+
+export function pipeLog<A>(a: A): A {
+  console.log(a)
+  return a
+}
