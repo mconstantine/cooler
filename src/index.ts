3d1b0dff36abe78a296464dcf9002ad1923ef14a
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-11-09 18:14:14 +0100

    Updated server index.

--- src/index.ts
+++ src/index.ts
@@ -7,34 +7,45 @@ import express from 'express'
 import path from 'path'
 import { getContext, subscriptionOptions } from './getContext'
 import http from 'http'
+import { pipe } from 'fp-ts/function'
+import { taskEither } from 'fp-ts'
 
-async function start() {
+async function start(): Promise<void> {
   dotenv.config()
-  await init()
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: getContext,
-    subscriptions: subscriptionOptions
-  })
+  await pipe(
+    init(),
+    taskEither.bimap(
+      error => Promise.reject(error),
+      () => {
+        const server = new ApolloServer({
+          typeDefs,
+          resolvers,
+          context: getContext,
+          subscriptions: subscriptionOptions
+        })
 
-  const app = express()
-  const httpServer = http.createServer(app)
+        const app = express()
+        const httpServer = http.createServer(app)
 
-  server.applyMiddleware({ app })
-  server.installSubscriptionHandlers(httpServer)
+        server.applyMiddleware({ app })
+        server.installSubscriptionHandlers(httpServer)
 
-  app
-    .use('/public', express.static(path.join(process.cwd(), '/public')))
-    .use('/', express.static(path.join(process.cwd(), '../cooler/build')))
-    .use('*', (_req, res) =>
-      res.sendFile(path.join(process.cwd(), '../cooler/build/index.html'))
+        app
+          .use('/public', express.static(path.join(process.cwd(), '/public')))
+          .use('/', express.static(path.join(process.cwd(), '../cooler/build')))
+          .use('*', (_req, res) =>
+            res.sendFile(path.join(process.cwd(), '../cooler/build/index.html'))
+          )
+
+        httpServer.listen({ port: process.env.SERVER_PORT }, () => {
+          console.log(
+            `Server ready at http://localhost:${process.env.SERVER_PORT}`
+          )
+        })
+      }
     )
-
-  httpServer.listen({ port: process.env.SERVER_PORT }, () => {
-    console.log(`Server ready at http://localhost:${process.env.SERVER_PORT}`)
-  })
+  )()
 }
 
 start().catch(e => console.log(e))
