7c1ac0f7976153ecd5e75bea2afd1e68cb5b42db
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-11-13 16:00:15 +0100

    Updated pubsub functions.

--- src/misc/pubsub.ts
+++ src/misc/pubsub.ts
@@ -1,29 +1,100 @@
-import { pubsub } from '../pubsub'
+import { ResolverFn, withFilter } from 'apollo-server-express'
+import { TaskEither } from 'fp-ts/TaskEither'
+import * as t from 'io-ts'
 import { Context, UserContext } from '../user/interface'
+import { PubSub } from 'apollo-server-express'
+import { constFalse, pipe } from 'fp-ts/function'
+import { either, task, taskEither } from 'fp-ts'
 
-export interface SubscriptionImplementation<T> {
-  subscribe: () => AsyncIterator<T>
+const pubsub = new PubSub()
+
+interface Subscription<
+  P,
+  C,
+  TI extends t.Type<any, any>,
+  TO extends t.Type<any, any>
+> {
+  argsCodec: TI
+  resultCodec: TO
+  key: string
+  filter: (
+    args: t.TypeOf<TI>,
+    parent: P,
+    context: C
+  ) => TaskEither<unknown, boolean>
+  publish: (result: t.TypeOf<TO>) => t.TypeOf<TO>
 }
 
-export type Subscription<T> = Record<string, SubscriptionImplementation<T>>
-
-export function publish<
-  T,
-  O extends Record<string, SubscriptionImplementation<T>>
->(triggerName: string, content: Partial<{ [key in keyof O]: T }>) {
-  return pubsub.publish(triggerName, content)
+interface Subscriptions {
+  [key: string]: Subscription<any, any, any, any>
 }
 
-export type WithFilter<
-  V extends Record<string, any>,
-  O extends Record<string, SubscriptionImplementation<T>> = Record<
-    string,
-    SubscriptionImplementation<any>
-  >,
+export function createSubscription<
+  P = any,
   C extends Context = UserContext,
-  T = any
-> = (
-  payload: { [key in keyof O]: T },
-  variables: V,
-  context: C
-) => boolean | Promise<boolean>
+  TI extends t.Type<any, any> = t.Type<any, any>,
+  TO extends t.Type<any, any> = t.Type<any, any>
+>(
+  argsCodec: TI,
+  resultCodec: TO,
+  key: string,
+  filter: (
+    parent: P,
+    args: t.TypeOf<TI>,
+    context: C
+  ) => TaskEither<unknown, boolean>
+): Subscription<P, C, TI, TO> {
+  return {
+    argsCodec,
+    resultCodec,
+    key,
+    filter,
+    publish: result => {
+      pipe(result, resultCodec.encode, (result: t.OutputOf<TO>) =>
+        pubsub.publish(key, result)
+      )
+
+      return result
+    }
+  }
+}
+
+export function createSubscriptions(
+  subscription: Subscriptions
+): {
+  [K in keyof Subscriptions]: ResolverFn
+} {
+  return Object.entries(subscription)
+    .map(([key, subscription]): [string, ResolverFn] => {
+      return [
+        key,
+        withFilter(
+          () => pubsub.asyncIterator([subscription.key]),
+          (parent, args, context) =>
+            pipe(
+              args,
+              subscription.argsCodec.decode,
+              either.fold(
+                () => task.fromIO(constFalse),
+                args =>
+                  pipe(
+                    subscription.filter(parent, args, context),
+                    taskEither.fold(
+                      () => task.fromIO(constFalse),
+                      result => task.fromIO(() => result)
+                    )
+                  )
+              ),
+              task => task()
+            )
+        )
+      ]
+    })
+    .reduce(
+      (res, [key, resolverFn]) => ({
+        ...res,
+        [key]: resolverFn
+      }),
+      {}
+    )
+}
