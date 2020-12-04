bcc78bb902ac73438f888db3c0f78ff54ed3b2d0
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-12-04 21:56:41 +0100

    Removed server build. gitignore is not working.

--- server/build/test/sleep.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-var fp_ts_1 = require("fp-ts");
-var function_1 = require("fp-ts/function");
-function sleep(millis, a) {
-    return function_1.pipe(fp_ts_1.taskEither.tryCatch(function () { return new Promise(function (done) { return setTimeout(done, millis); }); }, function_1.constVoid), fp_ts_1.taskEither.fold(function () { return fp_ts_1.task.fromIO(function () { return a; }); }, function () { return fp_ts_1.task.fromIO(function () { return a; }); }));
-}
-exports.sleep = sleep;
