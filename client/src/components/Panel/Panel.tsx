6472a3c5a78687402da0131bf4a20ed6f538801d
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-12-10 17:51:20 +0100

    Added Panel and Separator.

---
+++ client/src/components/Panel/Panel.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react'
+import './Panel.scss'
+
+export const Panel: FC = ({ children }) => (
+  <div className="Panel">{children}</div>
+)
---
+++ client/src/components/Separator/Separator.tsx
@@ -0,0 +1,4 @@
+import { FC } from 'react'
+import './Separator.scss'
+
+export const Separator: FC = () => <hr />
