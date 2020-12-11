0c5ff7497a850be929174b3db5cd959e0af4111a
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-12-11 21:14:54 +0100

    Added routed lists.

--- client/src/components/List/List.tsx
+++ client/src/components/List/List.tsx
@@ -1,42 +1,93 @@
 import { boolean, option } from 'fp-ts'
-import { constNull, pipe } from 'fp-ts/function'
+import { constNull, constUndefined, pipe } from 'fp-ts/function'
 import { Option } from 'fp-ts/Option'
-import { FC } from 'react'
+import { FC, MouseEvent } from 'react'
 import { Color, LocalizedString } from '../../globalDomain'
 import { composeClassName } from '../../misc/composeClassName'
 import { Icon } from '../Icon/Icon'
 import { Label } from '../Label/Label'
 import './List.scss'
+import * as t from 'io-ts'
+import { chevronForwardOutline } from 'ionicons/icons'
+
+const ListType = t.keyof(
+  {
+    readonly: true,
+    readonlyWithIcon: true,
+    routed: true,
+    routedWithIcon: true
+  },
+  'ListType'
+)
+type ListType = t.TypeOf<typeof ListType>
 
 interface ReadonlyItem {
+  key: string | number
   label: Option<LocalizedString>
   content: LocalizedString
   description: Option<LocalizedString>
+  disabled?: boolean
 }
 
-interface ItemWithIcon {
-  label: Option<LocalizedString>
-  content: LocalizedString
-  description: Option<LocalizedString>
+interface ReadonlyItemWithIcon {
   icon: string
   iconColor?: Color
 }
 
-type Item = ReadonlyItem | ItemWithIcon
+interface RoutedItem {
+  action: () => unknown
+}
+
+interface RoutedItemWithIcon {
+  icon: string
+  iconColor: Color
+  action: () => unknown
+}
+
+type Item = ReadonlyItem &
+  (ReadonlyItemWithIcon | RoutedItem | RoutedItemWithIcon)
 
 function foldItem<T>(
-  listType: 'readonly' | 'withIcons',
+  listType: ListType,
   whenReadonly: (item: ReadonlyItem) => T,
-  whenWithIcon: (item: ItemWithIcon) => T
+  whenWithIcon: (item: ReadonlyItemWithIcon) => T,
+  whenRouted: (item: RoutedItem) => T,
+  whenRoutedWithIcon: (item: RoutedItemWithIcon) => T
 ): (item: Item) => T {
   return item => {
     switch (listType) {
       case 'readonly':
         return whenReadonly(item as ReadonlyItem)
-      case 'withIcons':
-        return whenWithIcon(item as ItemWithIcon)
+      case 'readonlyWithIcon':
+        return whenWithIcon(item as ReadonlyItemWithIcon)
+      case 'routed':
+        return whenRouted(item as RoutedItem)
+      case 'routedWithIcon':
+        return whenRoutedWithIcon(item as RoutedItemWithIcon)
     }
   }
+}
+
+function foldItemIcon<T>(
+  listType: ListType,
+  whenNotWithIcon: (item: ReadonlyItem | RoutedItem) => T,
+  whenWithIcon: (item: ReadonlyItemWithIcon | RoutedItemWithIcon) => T
+): (item: Item) => T {
+  return foldItem(
+    listType,
+    whenNotWithIcon,
+    whenWithIcon,
+    whenNotWithIcon,
+    whenWithIcon
+  )
+}
+
+function foldItemRouted<T>(
+  listType: ListType,
+  whenReadonly: (item: ReadonlyItem | ReadonlyItemWithIcon) => T,
+  whenRouted: (item: RoutedItem | RoutedItemWithIcon) => T
+): (item: Item) => T {
+  return foldItem(listType, whenReadonly, whenReadonly, whenRouted, whenRouted)
 }
 
 type Props = {
@@ -48,9 +99,17 @@ type Props = {
       items: ReadonlyItem[]
     }
   | {
-      type: 'withIcons'
+      type: 'readonlyWithIcon'
       iconsPosition: 'start' | 'end'
-      items: ItemWithIcon[]
+      items: ReadonlyItemWithIcon[]
+    }
+  | {
+      type: 'routed'
+      items: RoutedItem[]
+    }
+  | {
+      type: 'routedWithIcon'
+      items: RoutedItemWithIcon[]
     }
 )
 
@@ -69,46 +128,86 @@ export const List: FC<Props> = ({
   )
 
   const iconsAtTheEndClassName =
-    props.type === 'withIcons' && props.iconsPosition === 'end'
+    props.type === 'readonlyWithIcon' && props.iconsPosition === 'end'
       ? 'iconsAtTheEnd'
       : ''
 
+  const routedClassName =
+    props.type === 'routed' || props.type === 'routedWithIcon' ? 'routed' : ''
+
   return (
-    <div className={composeClassName('List', unwrap, iconsAtTheEndClassName)}>
+    <div
+      className={composeClassName(
+        'List',
+        routedClassName,
+        unwrap,
+        iconsAtTheEndClassName
+      )}
+    >
       {pipe(
         heading,
         option.map(heading => <h5 className="heading">{heading}</h5>),
         option.toNullable
       )}
       <ul>
-        {(items as Item[]).map((item: Item) => (
-          <li>
-            <div className="itemContentWrapper">
+        {(items as Item[]).map(item => (
+          <li
+            key={item.key}
+            onClick={pipe(
+              item,
+              foldItemRouted(
+                props.type,
+                constUndefined,
+                ({ action }) => (e: MouseEvent) => {
+                  e.preventDefault()
+
+                  if (item.disabled) {
+                    return
+                  }
+
+                  return action()
+                }
+              )
+            )}
+            className={composeClassName(item.disabled ? 'disabled' : '')}
+          >
+            <div className="itemContentOuterWrapper">
+              <div className="itemContentWrapper">
+                {pipe(
+                  item,
+                  foldItemIcon(
+                    props.type,
+                    constNull,
+                    ({ icon, iconColor = 'default' }) => (
+                      <Icon color={iconColor} src={icon} />
+                    )
+                  )
+                )}
+                <div className="itemContent">
+                  {pipe(
+                    item.label,
+                    option.map(label => <Label content={label} />),
+                    option.toNullable
+                  )}
+                  <h6 className="content">{item.content}</h6>
+                  {pipe(
+                    item.description,
+                    option.map(description => (
+                      <p className="description">{description}</p>
+                    )),
+                    option.toNullable
+                  )}
+                </div>
+              </div>
+
               {pipe(
                 item,
-                foldItem(
-                  props.type,
-                  constNull,
-                  ({ icon, iconColor = 'default' }) => (
-                    <Icon color={iconColor} src={icon} />
-                  )
-                )
+                foldItemRouted(props.type, constNull, () => (
+                  <div className="routeArrow">
+                    <Icon src={chevronForwardOutline} size="medium" />
+                  </div>
+                ))
               )}
-              <div className="itemContent">
-                {pipe(
-                  item.label,
-                  option.map(label => <Label content={label} />),
-                  option.toNullable
-                )}
-                <h6 className="content">{item.content}</h6>
-                {pipe(
-                  item.description,
-                  option.map(description => (
-                    <p className="description">{description}</p>
-                  )),
-                  option.toNullable
-                )}
-              </div>
             </div>
           </li>
         ))}
--- client/src/stories/List.stories.tsx
+++ client/src/stories/List.stories.tsx
@@ -1,157 +1,340 @@
 import { Meta, Story } from '@storybook/react'
 import { option } from 'fp-ts'
-import { checkmark, heart, skull, star, warning } from 'ionicons/icons'
+import {
+  batteryDead,
+  checkmark,
+  heart,
+  skull,
+  star,
+  warning
+} from 'ionicons/icons'
 import { unsafeLocalizedString } from '../a18n'
 import { Content } from '../components/Content/Content'
 import { List } from '../components/List/List'
 import { CoolerStory } from './CoolerStory'
 
-export const Readonly: Story = args => (
-  <CoolerStory>
-    <Content>
-      <List
-        type="readonly"
-        heading={option.some(unsafeLocalizedString('Read-only list'))}
-        unwrapDescriptions={args.unwrapDescriptions}
-        items={[
-          {
-            label: option.none,
-            content: unsafeLocalizedString('Content only item'),
-            description: option.none
-          },
-          {
-            label: option.some(unsafeLocalizedString('Label')),
-            content: unsafeLocalizedString('Label and content'),
-            description: option.none
-          },
-          {
-            label: option.some(unsafeLocalizedString('Another label')),
-            content: unsafeLocalizedString('Label and content and description'),
-            description: option.some(
-              unsafeLocalizedString(
-                'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis autem ex vel aliquid enim commodi laudantium esse eos eligendi ullam, ratione ipsam iste dolorem consectetur nesciunt at incidunt eveniet hic.'
-              )
-            )
-          }
-        ]}
-      />
-      <List
-        type="withIcons"
-        iconsPosition="start"
-        heading={option.some(
-          unsafeLocalizedString('List with icons at the start')
-        )}
-        unwrapDescriptions={args.unwrapDescriptions}
-        items={[
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the start')
-            ),
-            content: unsafeLocalizedString('Default icon color'),
-            description: option.none,
-            icon: star
-          },
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the start')
-            ),
-            content: unsafeLocalizedString('Primary icon color'),
-            description: option.none,
-            icon: heart,
-            iconColor: 'primary'
-          },
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the start')
-            ),
-            content: unsafeLocalizedString('Success icon color'),
-            description: option.none,
-            icon: checkmark,
-            iconColor: 'success'
-          },
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the start')
-            ),
-            content: unsafeLocalizedString('Warning icon color'),
-            description: option.none,
-            icon: warning,
-            iconColor: 'warning'
-          },
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the start')
-            ),
-            content: unsafeLocalizedString('Danger icon color'),
-            description: option.none,
-            icon: skull,
-            iconColor: 'danger'
-          }
-        ]}
-      />
-      <List
-        type="withIcons"
-        iconsPosition="end"
-        heading={option.some(
-          unsafeLocalizedString('List with icons at the end')
-        )}
-        unwrapDescriptions={args.unwrapDescriptions}
-        items={[
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the end')
-            ),
-            content: unsafeLocalizedString('Default icon color'),
-            description: option.none,
-            icon: star
-          },
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the end')
-            ),
-            content: unsafeLocalizedString('Primary icon color'),
-            description: option.none,
-            icon: heart,
-            iconColor: 'primary'
-          },
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the end')
-            ),
-            content: unsafeLocalizedString('Success icon color'),
-            description: option.none,
-            icon: checkmark,
-            iconColor: 'success'
-          },
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the end')
-            ),
-            content: unsafeLocalizedString('Warning icon color'),
-            description: option.none,
-            icon: warning,
-            iconColor: 'warning'
-          },
-          {
-            label: option.some(
-              unsafeLocalizedString('Item with icon at the end')
-            ),
-            content: unsafeLocalizedString('Danger icon color'),
-            description: option.none,
-            icon: skull,
-            iconColor: 'danger'
-          }
-        ]}
-      />
-    </Content>
-  </CoolerStory>
+const description = unsafeLocalizedString(
+  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis autem ex vel aliquid enim commodi laudantium esse eos eligendi ullam, ratione ipsam iste dolorem consectetur nesciunt at incidunt eveniet hic.'
 )
+
+export const Readonly: Story = ({ unwrapDescriptions }) => {
+  return (
+    <CoolerStory>
+      <Content>
+        <List
+          type="readonly"
+          heading={option.some(unsafeLocalizedString('Read-only list'))}
+          unwrapDescriptions={unwrapDescriptions}
+          items={[
+            {
+              key: 0,
+              label: option.none,
+              content: unsafeLocalizedString('Content only item'),
+              description: option.none
+            },
+            {
+              key: 1,
+              label: option.some(unsafeLocalizedString('Label')),
+              content: unsafeLocalizedString('Label and content'),
+              description: option.none
+            },
+            {
+              key: 2,
+              label: option.some(unsafeLocalizedString('Another label')),
+              content: unsafeLocalizedString(
+                'Label and content and description'
+              ),
+              description: option.some(description)
+            },
+            {
+              key: 3,
+              label: option.none,
+              content: unsafeLocalizedString('Disabled'),
+              description: option.none,
+              disabled: true
+            }
+          ]}
+        />
+        <List
+          type="readonlyWithIcon"
+          iconsPosition="start"
+          heading={option.some(
+            unsafeLocalizedString('Read-only list with icons at the start')
+          )}
+          unwrapDescriptions={unwrapDescriptions}
+          items={[
+            {
+              key: 0,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Default icon color'),
+              description: option.none,
+              icon: star,
+              iconColor: 'default'
+            },
+            {
+              key: 1,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Primary icon color'),
+              description: option.none,
+              icon: heart,
+              iconColor: 'primary'
+            },
+            {
+              key: 2,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Success icon color'),
+              description: option.none,
+              icon: checkmark,
+              iconColor: 'success'
+            },
+            {
+              key: 3,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Warning icon color'),
+              description: option.none,
+              icon: warning,
+              iconColor: 'warning'
+            },
+            {
+              key: 4,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Danger icon color'),
+              description: option.none,
+              icon: skull,
+              iconColor: 'danger'
+            },
+            {
+              key: 5,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Disabled'),
+              description: option.none,
+              icon: batteryDead,
+              iconColor: 'danger',
+              disabled: true
+            }
+          ]}
+        />
+        <List
+          type="readonlyWithIcon"
+          iconsPosition="end"
+          heading={option.some(
+            unsafeLocalizedString('Read-only list with icons at the end')
+          )}
+          unwrapDescriptions={unwrapDescriptions}
+          items={[
+            {
+              key: 0,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the end')
+              ),
+              content: unsafeLocalizedString('Default icon color'),
+              description: option.none,
+              icon: star,
+              iconColor: 'default'
+            },
+            {
+              key: 1,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the end')
+              ),
+              content: unsafeLocalizedString('Primary icon color'),
+              description: option.none,
+              icon: heart,
+              iconColor: 'primary'
+            },
+            {
+              key: 2,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the end')
+              ),
+              content: unsafeLocalizedString('Success icon color'),
+              description: option.some(description),
+              icon: checkmark,
+              iconColor: 'success'
+            },
+            {
+              key: 3,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the end')
+              ),
+              content: unsafeLocalizedString('Warning icon color'),
+              description: option.some(description),
+              icon: warning,
+              iconColor: 'warning'
+            },
+            {
+              key: 4,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the end')
+              ),
+              content: unsafeLocalizedString('Danger icon color'),
+              description: option.some(description),
+              icon: skull,
+              iconColor: 'danger'
+            },
+            {
+              key: 5,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the end')
+              ),
+              content: unsafeLocalizedString('Disabled'),
+              description: option.none,
+              icon: batteryDead,
+              iconColor: 'danger',
+              disabled: true
+            }
+          ]}
+        />
+      </Content>
+    </CoolerStory>
+  )
+}
+
+export const Routed: Story = ({ unwrapDescriptions, onClick }) => {
+  return (
+    <CoolerStory>
+      <Content>
+        <List
+          type="routed"
+          heading={option.some(unsafeLocalizedString('Routed list'))}
+          unwrapDescriptions={unwrapDescriptions}
+          items={[
+            {
+              key: 0,
+              label: option.none,
+              content: unsafeLocalizedString('Content only item'),
+              description: option.none,
+              action: () => onClick(0)
+            },
+            {
+              key: 1,
+              label: option.some(unsafeLocalizedString('Label')),
+              content: unsafeLocalizedString('Label and content'),
+              description: option.none,
+              action: () => onClick(1)
+            },
+            {
+              key: 2,
+              label: option.some(unsafeLocalizedString('Another label')),
+              content: unsafeLocalizedString(
+                'Label and content and description'
+              ),
+              description: option.some(description),
+              action: () => onClick(2)
+            },
+            {
+              key: 3,
+              label: option.none,
+              content: unsafeLocalizedString('Disabled'),
+              description: option.none,
+              action: () => onClick(2),
+              disabled: true
+            }
+          ]}
+        />
+        <List
+          type="routedWithIcon"
+          heading={option.some(
+            unsafeLocalizedString('Routed list with icons at the start')
+          )}
+          unwrapDescriptions={unwrapDescriptions}
+          items={[
+            {
+              key: 0,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Default icon color'),
+              description: option.none,
+              icon: star,
+              iconColor: 'default',
+              action: () => onClick(0)
+            },
+            {
+              key: 1,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Primary icon color'),
+              description: option.none,
+              icon: heart,
+              iconColor: 'primary',
+              action: () => onClick(1)
+            },
+            {
+              key: 2,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Success icon color'),
+              description: option.some(description),
+              icon: checkmark,
+              iconColor: 'success',
+              action: () => onClick(2)
+            },
+            {
+              key: 3,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Warning icon color'),
+              description: option.some(description),
+              icon: warning,
+              iconColor: 'warning',
+              action: () => onClick(3)
+            },
+            {
+              key: 4,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Danger icon color'),
+              description: option.some(description),
+              icon: skull,
+              iconColor: 'danger',
+              action: () => onClick(4)
+            },
+            {
+              key: 5,
+              label: option.some(
+                unsafeLocalizedString('Item with icon at the start')
+              ),
+              content: unsafeLocalizedString('Disabled'),
+              description: option.none,
+              icon: batteryDead,
+              iconColor: 'danger',
+              action: () => onClick(5),
+              disabled: true
+            }
+          ]}
+        />
+      </Content>
+    </CoolerStory>
+  )
+}
 
 const meta: Meta = {
   title: 'Cooler/List',
   component: Readonly,
   args: {
     unwrapDescriptions: false
+  },
+  argTypes: {
+    onClick: {
+      action: 'clicked'
+    }
   }
 }
 
