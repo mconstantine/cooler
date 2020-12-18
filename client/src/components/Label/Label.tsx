09eada590e9255db64a17e9ea2edf352b5fe7cff
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-12-18 16:55:25 +0100

    Added icon and color to Label.

--- client/src/components/Label/Label.tsx
+++ client/src/components/Label/Label.tsx
@@ -1,11 +1,20 @@
 import { FC } from 'react'
-import { LocalizedString } from '../../globalDomain'
+import { Color, LocalizedString } from '../../globalDomain'
+import { composeClassName } from '../../misc/composeClassName'
+import { Icon } from '../Icon/Icon'
 import './Label.scss'
 
 interface Props {
   content: LocalizedString
+  icon?: string
+  color?: Color
 }
 
-export const Label: FC<Props> = ({ content }) => {
-  return <p className="Label">{content}</p>
+export const Label: FC<Props> = ({ content, icon, color = 'default' }) => {
+  return (
+    <p className={composeClassName('Label', color)}>
+      {icon ? <Icon color={color} src={icon} size="small" /> : null}
+      {content}
+    </p>
+  )
 }
