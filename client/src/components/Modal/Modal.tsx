61fc9cc843026c79ef9884a2b31211b0ef169c32
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-12-20 00:42:51 +0100

    Added Modal.

---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,40 @@
+import { FC, useEffect } from 'react'
+import { createPortal } from 'react-dom'
+import { composeClassName } from '../../misc/composeClassName'
+import { Panel } from '../Panel/Panel'
+import './Modal.scss'
+
+interface Props {
+  isOpen: boolean
+  onClose: () => void
+}
+
+export const Modal: FC<Props> = ({ isOpen, onClose, children }) => {
+  const openClassName = isOpen ? 'open' : ''
+
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.code === 'Escape') {
+        onClose()
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener('keyup', onKeyUp)
+    } else {
+      document.removeEventListener('keyup', onKeyUp)
+    }
+
+    return () => {
+      document.removeEventListener('keyup', onKeyUp)
+    }
+  }, [isOpen, onClose])
+
+  return createPortal(
+    <div className={composeClassName('Modal', openClassName)}>
+      <div className="dim" onClick={onClose} />
+      <Panel>{children}</Panel>
+    </div>,
+    document.body
+  )
+}
---
+++ client/src/stories/Modal.stories.tsx
@@ -0,0 +1,100 @@
+import { Meta, Story } from '@storybook/react'
+import { option } from 'fp-ts'
+import { useState } from 'react'
+import { unsafeLocalizedString } from '../a18n'
+import { Button } from '../components/Button/Button/Button'
+import { Content } from '../components/Content/Content'
+import { List } from '../components/List/List'
+import { Modal as ModalComponent } from '../components/Modal/Modal'
+import { Separator } from '../components/Separator/Separator'
+import { CoolerStory } from './CoolerStory'
+
+export const Modal: Story = ({ onChooseItem }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  return (
+    <CoolerStory>
+      <Content>
+        <ModalComponent
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        >
+          <List
+            type="routed"
+            heading={option.none}
+            items={new Array(30).fill(null).map((_, index) => ({
+              key: index,
+              label: option.none,
+              content: unsafeLocalizedString('Some list item'),
+              description: option.none,
+              action: () => {
+                onChooseItem(`Item ${index + 1}`)
+                setIsModalOpen(false)
+              }
+            }))}
+          />
+        </ModalComponent>
+
+        <Button
+          type="button"
+          label={unsafeLocalizedString('Open the modal')}
+          action={() => setIsModalOpen(true)}
+          icon={option.none}
+        />
+
+        <Separator />
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea eum vel
+          porro recusandae nesciunt itaque pariatur nemo omnis consequuntur
+          fuga? Quaerat molestiae quo sit tenetur, voluptatibus animi modi id
+          quae vero suscipit, perspiciatis maiores veritatis sed earum
+          praesentium quod reprehenderit qui? Error quod suscipit placeat
+          perferendis deleniti ipsa assumenda? Eaque in porro accusantium, atque
+          veniam praesentium, aliquid molestiae delectus corporis necessitatibus
+          nostrum eos est accusamus illum vero deleniti quis blanditiis at nam
+          aperiam earum ipsa? Suscipit dignissimos laborum quis amet vitae
+          possimus fugit dicta, culpa deserunt facilis harum accusamus porro
+          ipsam inventore ducimus recusandae magni veritatis natus aliquid
+          tenetur adipisci. Dolore inventore earum iure delectus quasi.
+          Blanditiis nostrum nisi in quibusdam quis! Reprehenderit molestias
+          magni saepe cum perspiciatis cumque nihil, ullam cupiditate facere
+          voluptas vitae tempora. Ipsa excepturi velit quisquam neque in nam
+          magnam, sunt libero unde repellat voluptatem sint quis reprehenderit
+          doloremque, fugiat sapiente dicta, laborum laboriosam qui reiciendis.
+        </p>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea eum vel
+          porro recusandae nesciunt itaque pariatur nemo omnis consequuntur
+          fuga? Quaerat molestiae quo sit tenetur, voluptatibus animi modi id
+          quae vero suscipit, perspiciatis maiores veritatis sed earum
+          praesentium quod reprehenderit qui? Error quod suscipit placeat
+          perferendis deleniti ipsa assumenda? Eaque in porro accusantium, atque
+          veniam praesentium, aliquid molestiae delectus corporis necessitatibus
+          nostrum eos est accusamus illum vero deleniti quis blanditiis at nam
+          aperiam earum ipsa? Suscipit dignissimos laborum quis amet vitae
+          possimus fugit dicta, culpa deserunt facilis harum accusamus porro
+          ipsam inventore ducimus recusandae magni veritatis natus aliquid
+          tenetur adipisci. Dolore inventore earum iure delectus quasi.
+          Blanditiis nostrum nisi in quibusdam quis! Reprehenderit molestias
+          magni saepe cum perspiciatis cumque nihil, ullam cupiditate facere
+          voluptas vitae tempora. Ipsa excepturi velit quisquam neque in nam
+          magnam, sunt libero unde repellat voluptatem sint quis reprehenderit
+          doloremque, fugiat sapiente dicta, laborum laboriosam qui reiciendis.
+        </p>
+      </Content>
+    </CoolerStory>
+  )
+}
+
+const meta: Meta = {
+  title: 'Cooler/Modal',
+  argTypes: {
+    onChooseItem: {
+      action: 'item chosen'
+    }
+  }
+}
+
+export default meta
