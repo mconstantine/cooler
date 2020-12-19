e7c92d50b5f4134e721188de925c805cd481de73
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2020-12-19 14:06:57 +0100

    Added spacing to Buttons.

--- client/src/components/Button/Buttons/Buttons.tsx
+++ client/src/components/Button/Buttons/Buttons.tsx
@@ -1,11 +1,45 @@
+import { pipe } from 'fp-ts/function'
 import { ComponentProps, FC, ReactElement } from 'react'
+import { composeClassName } from '../../../misc/composeClassName'
 import { Button } from '../Button/Button'
 import './Buttons.scss'
 
+type Spacing = 'start' | 'spread'
+
 interface Props {
-  children: ReactElement<ComponentProps<typeof Button>>[]
+  children: Array<ReactElement<ComponentProps<typeof Button>>>
+  spacing?: Spacing
 }
 
-export const Buttons: FC<Props> = ({ children }) => {
-  return <div className="Buttons">{children}</div>
+function foldSpacing<T>(
+  whenStart: () => T,
+  whenSpread: () => T
+): (spacing: Spacing) => T {
+  return spacing => {
+    switch (spacing) {
+      case 'start':
+        return whenStart()
+      case 'spread':
+        return whenSpread()
+    }
+  }
+}
+
+export const Buttons: FC<Props> = ({ children, spacing = 'start' }) => {
+  return (
+    <div className={composeClassName('Buttons', spacing)}>
+      {pipe(
+        spacing,
+        foldSpacing<any>(
+          () => children,
+          () => (
+            <>
+              <div>{children[0]}</div>
+              <div>{children.slice(1)}</div>
+            </>
+          )
+        )
+      )}
+    </div>
+  )
 }
