ffe71b36087e334f2714449a14c126f22706d0d2
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2021-01-29 19:24:53 +0100

    Added text emphasys to Body.

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -3,14 +3,20 @@ import { Color, LocalizedString } from '../../globalDomain'
 import { composeClassName } from '../../misc/composeClassName'
 import './Body.scss'
 
+export type TextEmphasis = 'full' | 'high' | 'medium' | 'low'
+
 interface Props {
   color?: Color
+  emphasis?: TextEmphasis
   children: LocalizedString
 }
 
 export const Body: FC<Props> = props => {
+  const color = props.color || 'default'
+  const emphasis = props.emphasis || 'full'
+
   return (
-    <p className={composeClassName('Body', props.color || 'default')}>
+    <p className={composeClassName('Body', color, emphasis)}>
       {props.children}
     </p>
   )
