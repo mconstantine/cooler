929a3afc98e1e14dc65cc93c4efe920556fd0689
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2021-01-29 18:59:18 +0100

    Isolated UserForm.

---
+++ client/src/components/Form/Forms/UserForm.tsx
@@ -0,0 +1,130 @@
+import { option, taskEither } from 'fp-ts'
+import { sequenceS } from 'fp-ts/Apply'
+import { flow, pipe } from 'fp-ts/function'
+import { IO } from 'fp-ts/IO'
+import { Option } from 'fp-ts/Option'
+import { TaskEither } from 'fp-ts/TaskEither'
+import { NonEmptyString } from 'io-ts-types'
+import { FC } from 'react'
+import { a18n } from '../../../a18n'
+import { EmailString, LocalizedString } from '../../../globalDomain'
+import { commonErrors } from '../../../misc/commonErrors'
+import { Form } from '../Form'
+import { Input } from '../Input/Input/Input'
+import { useForm } from '../useForm'
+import * as validators from '../validators'
+
+interface UserData {
+  name: LocalizedString
+  email: EmailString
+  created_at: Date
+  updated_at: Date
+}
+
+interface UserUpdate extends UserData {
+  newPassword: Option<NonEmptyString>
+}
+
+interface Props {
+  user: UserData
+  onSubmit: (data: UserUpdate) => TaskEither<LocalizedString, unknown>
+  onCancel: IO<void>
+}
+
+export const UserForm: FC<Props> = props => {
+  const { fieldProps, formError, submit } = useForm(
+    {
+      initialValues: {
+        name: props.user.name as string,
+        email: props.user.email as string,
+        newPassword: '',
+        passwordConfirmation: '',
+        created_at: props.user.created_at,
+        updated_at: props.user.updated_at
+      },
+      validators: () => ({
+        name: validators.fromCodec(NonEmptyString, commonErrors.nonBlank),
+        email: validators.fromCodec(EmailString, commonErrors.invalidEmail),
+        newPassword: validators.optionalString(),
+        passwordConfirmation: validators.optionalString()
+      }),
+      linters: () => ({})
+    },
+    {
+      formValidator: data => {
+        const defaultData = {
+          name: (data.name as string) as LocalizedString,
+          email: data.email,
+          newPassword: option.none,
+          created_at: data.created_at,
+          updated_at: data.updated_at
+        }
+
+        return pipe(
+          sequenceS(option.option)({
+            password: data.newPassword,
+            passwordConfirmation: data.passwordConfirmation
+          }),
+          option.fold(
+            () =>
+              taskEither.right({
+                ...defaultData,
+                newPassword: option.none
+              }),
+            flow(
+              taskEither.fromPredicate(
+                ({ password, passwordConfirmation }) =>
+                  password === passwordConfirmation,
+                () => a18n`Passwords don't match`
+              ),
+              taskEither.map(({ password }) => ({
+                ...defaultData,
+                newPassword: option.some(password)
+              }))
+            )
+          )
+        )
+      }
+    },
+    {
+      onSubmit: data => props.onSubmit(data)
+    }
+  )
+
+  return (
+    <Form
+      title={a18n`Edit your data`}
+      formError={formError}
+      submit={submit}
+      additionalButtons={[
+        {
+          type: 'button',
+          label: a18n`Cancel`,
+          icon: option.none,
+          action: props.onCancel,
+          flat: true
+        }
+      ]}
+    >
+      <Input label={a18n`Name`} {...fieldProps('name')} autoComplete="name" />
+      <Input
+        type="email"
+        label={a18n`E-mail address`}
+        {...fieldProps('email')}
+        autoComplete="email"
+      />
+      <Input
+        type="password"
+        label={a18n`New password`}
+        {...fieldProps('newPassword')}
+        autoComplete="new-password"
+      />
+      <Input
+        type="password"
+        label={a18n`New password (again)`}
+        {...fieldProps('passwordConfirmation')}
+        autoComplete="new-password"
+      />
+    </Form>
+  )
+}
--- client/src/components/Pages/Profile/UserData.tsx
+++ client/src/components/Pages/Profile/UserData.tsx
@@ -1,16 +1,10 @@
 import { boolean, option, taskEither } from 'fp-ts'
 import { flow, pipe } from 'fp-ts/function'
-import { sequenceS } from 'fp-ts/Apply'
 import { TaskEither } from 'fp-ts/TaskEither'
 import { NonEmptyString } from 'io-ts-types'
 import { FC, useState } from 'react'
 import { a18n, formatDateTime } from '../../../a18n'
 import { EmailString, LocalizedString } from '../../../globalDomain'
-import { commonErrors } from '../../../misc/commonErrors'
-import { Form } from '../../Form/Form'
-import { Input } from '../../Form/Input/Input/Input'
-import { useForm } from '../../Form/useForm'
-import * as validators from '../../Form/validators'
 import { Option } from 'fp-ts/Option'
 import { Panel } from '../../Panel/Panel'
 import { List } from '../../List/List'
@@ -18,6 +12,7 @@ import { Buttons } from '../../Button/Buttons/Buttons'
 import { Button } from '../../Button/Button/Button'
 import { LoadingButton } from '../../Button/LoadingButton/LoadingButton'
 import { skull } from 'ionicons/icons'
+import { UserForm } from '../../Form/Forms/UserForm'
 
 interface UserData {
   name: LocalizedString
@@ -40,67 +35,12 @@ interface Props {
 export const UserData: FC<Props> = props => {
   const [isEditing, setIsEditing] = useState(false)
 
-  const { fieldProps, formError, submit } = useForm(
-    {
-      initialValues: {
-        name: props.user.name as string,
-        email: props.user.email as string,
-        newPassword: '',
-        passwordConfirmation: '',
-        created_at: props.user.created_at,
-        updated_at: props.user.updated_at
-      },
-      validators: () => ({
-        name: validators.fromCodec(NonEmptyString, commonErrors.nonBlank),
-        email: validators.fromCodec(EmailString, commonErrors.invalidEmail),
-        newPassword: validators.optionalString(),
-        passwordConfirmation: validators.optionalString()
-      }),
-      linters: () => ({})
-    },
-    {
-      formValidator: data => {
-        const defaultData = {
-          name: (data.name as string) as LocalizedString,
-          email: data.email,
-          newPassword: option.none,
-          created_at: data.created_at,
-          updated_at: data.updated_at
-        }
-
-        return pipe(
-          sequenceS(option.option)({
-            password: data.newPassword,
-            passwordConfirmation: data.passwordConfirmation
-          }),
-          option.fold(
-            () =>
-              taskEither.right({
-                ...defaultData,
-                newPassword: option.none
-              }),
-            flow(
-              taskEither.fromPredicate(
-                ({ password, passwordConfirmation }) =>
-                  password === passwordConfirmation,
-                () => a18n`Passwords don't match`
-              ),
-              taskEither.map(({ password }) => ({
-                ...defaultData,
-                newPassword: option.some(password)
-              }))
-            )
-          )
-        )
-      }
-    },
-    {
-      onSubmit: flow(
-        props.onDataChange,
-        taskEither.chain(() => taskEither.fromIO(() => setIsEditing(false)))
-      )
-    }
+  const onSubmit = flow(
+    props.onDataChange,
+    taskEither.chain(() => taskEither.fromIO(() => setIsEditing(false)))
   )
+
+  const onCancel = () => setIsEditing(false)
 
   return pipe(
     isEditing,
@@ -167,44 +107,7 @@ export const UserData: FC<Props> = props => {
         </Panel>
       ),
       () => (
-        <Form
-          title={a18n`Edit your data`}
-          formError={formError}
-          submit={submit}
-          additionalButtons={[
-            {
-              type: 'button',
-              label: a18n`Cancel`,
-              icon: option.none,
-              action: () => setIsEditing(false),
-              flat: true
-            }
-          ]}
-        >
-          <Input
-            label={a18n`Name`}
-            {...fieldProps('name')}
-            autoComplete="name"
-          />
-          <Input
-            type="email"
-            label={a18n`E-mail address`}
-            {...fieldProps('email')}
-            autoComplete="email"
-          />
-          <Input
-            type="password"
-            label={a18n`New password`}
-            {...fieldProps('newPassword')}
-            autoComplete="new-password"
-          />
-          <Input
-            type="password"
-            label={a18n`New password (again)`}
-            {...fieldProps('passwordConfirmation')}
-            autoComplete="new-password"
-          />
-        </Form>
+        <UserForm user={props.user} onSubmit={onSubmit} onCancel={onCancel} />
       )
     )
   )
