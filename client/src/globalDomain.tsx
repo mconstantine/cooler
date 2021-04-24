bf0f716eebed4a2cc851d0842560aa2577cf8ef4
mconstantine <mauro.constantinescu@gmail.com>
2021-04-24 17:30:26 +0200

    Percentages can be greater than one.

--- client/src/globalDomain.tsx
+++ client/src/globalDomain.tsx
@@ -201,7 +201,7 @@ interface PercentageBrand {
 
 export const Percentage = t.brand(
   t.number,
-  (n): n is t.Branded<number, PercentageBrand> => n >= 0 && n <= 1,
+  (n): n is t.Branded<number, PercentageBrand> => n >= 0,
   'Percentage'
 )
 export type Percentage = t.TypeOf<typeof Percentage>
