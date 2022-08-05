e498a9e29e311aff4bae9945c7bdafe659a6e3fd
mauro <mauro@genuino.world>
2022-08-05 17:24:33 +0200

    Deploy script was a dumb idea.

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
 # I know nothing about bash, don't judge me :unicorn-face:
 
+echo "Setting environment variables..."
+
+while read -r line;
+do
+if [[ $line == name* ]]; then
+  app_name=`echo $line | awk -F' *:= *' '{print $2}'`
+elif [[ $line == version* ]]; then
+  app_version=`echo $line | awk -F' *:= *' '{print $2}'`
+fi
+done < ./server/build.sbt
+
+sed "s/APP_NAME=.*/APP_NAME=$app_name/g" .env > .env.tmp
+mv .env.tmp .env
+sed "s/APP_VERSION=.*/APP_VERSION=$app_version/g" .env > .env.tmp
+mv .env.tmp .env
+
 set -a
 source .env
 set +a
