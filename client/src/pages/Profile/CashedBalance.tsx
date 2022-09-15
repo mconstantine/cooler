09faeabe7bfc572c9a323ab30729d175341f48f7
mauro <mauro@genuino.world>
2022-09-15 17:33:33 +0200

    Added taxes amount to cash balance component.

--- client/src/pages/Profile/CashedBalance.tsx
+++ client/src/pages/Profile/CashedBalance.tsx
@@ -40,55 +40,71 @@ export function CashedBalance() {
     query.fold(
       () => <LoadingBlock />,
       error => <ErrorPanel error={error} />,
-      ({ balance, taxes }) => (
-        <Panel title={a18n`Cashed balance`} framed actions={option.none}>
-          <DateTimePicker
-            name="cashedBalanceSince"
-            mode="date"
-            label={a18n`Since`}
-            value={input.since}
-            onChange={since => setInput(input => ({ ...input, since }))}
-            error={option.none}
-            warning={option.none}
-            disabled={query.isLoading(cashedBalance)}
-          />
-          <DateTimePicker
-            name="cashedBalanceTo"
-            mode="date"
-            label={a18n`Until`}
-            value={input.to}
-            onChange={to => setInput(input => ({ ...input, to }))}
-            error={option.none}
-            warning={option.none}
-            disabled={query.isLoading(cashedBalance)}
-          />
-          <List
-            heading={option.none}
-            items={[
-              {
-                key: 'grossCashedAmount',
-                type: 'valued',
-                label: option.none,
-                content: a18n`Cashed amount (gross)`,
-                description: option.none,
-                value: formatMoneyAmount(balance),
-                progress: option.none
-              },
-              ...taxes.map(tax => renderTaxItem('cashedAmount', balance, tax)),
-              {
-                key: 'netCashedAmount',
-                type: 'valued',
-                label: option.none,
-                content: a18n`Cashed amount (net)`,
-                description: option.none,
-                value: formatMoneyAmount(calculateNetValue(balance, taxes)),
-                progress: option.none
-              }
-            ]}
-            emptyListMessage={unsafeLocalizedString('')}
-          />
-        </Panel>
-      )
+      ({ balance, taxes }) => {
+        const netAmount = calculateNetValue(balance, taxes)
+        const taxesAmount = balance - netAmount
+
+        return (
+          <Panel title={a18n`Cashed balance`} framed actions={option.none}>
+            <DateTimePicker
+              name="cashedBalanceSince"
+              mode="date"
+              label={a18n`Since`}
+              value={input.since}
+              onChange={since => setInput(input => ({ ...input, since }))}
+              error={option.none}
+              warning={option.none}
+              disabled={query.isLoading(cashedBalance)}
+            />
+            <DateTimePicker
+              name="cashedBalanceTo"
+              mode="date"
+              label={a18n`Until`}
+              value={input.to}
+              onChange={to => setInput(input => ({ ...input, to }))}
+              error={option.none}
+              warning={option.none}
+              disabled={query.isLoading(cashedBalance)}
+            />
+            <List
+              heading={option.none}
+              items={[
+                {
+                  key: 'grossCashedAmount',
+                  type: 'valued',
+                  label: option.none,
+                  content: a18n`Cashed amount (gross)`,
+                  description: option.none,
+                  value: formatMoneyAmount(balance),
+                  progress: option.none
+                },
+                ...taxes.map(tax =>
+                  renderTaxItem('cashedAmount', balance, tax)
+                ),
+                {
+                  key: 'netCashedAmount',
+                  type: 'valued',
+                  label: option.none,
+                  content: a18n`Cashed amount (net)`,
+                  description: option.none,
+                  value: formatMoneyAmount(netAmount),
+                  progress: option.none
+                },
+                {
+                  key: 'taxesAmount',
+                  type: 'valued',
+                  label: option.none,
+                  content: a18n`Taxes amount`,
+                  description: option.none,
+                  value: formatMoneyAmount(taxesAmount),
+                  progress: option.none
+                }
+              ]}
+              emptyListMessage={unsafeLocalizedString('')}
+            />
+          </Panel>
+        )
+      }
     )
   )
 }
