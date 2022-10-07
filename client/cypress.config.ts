c7ae36e65d959efb485926c95f433551ff27f14f
mauro <mauro@genuino.world>
2022-10-07 12:37:28 +0200

    Updated Cypress.

---
+++ client/cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  env: {
+    apiUrl: 'http://localhost:5000/graphql',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+})
